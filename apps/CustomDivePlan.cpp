3afd69aae61a5dc519c124136b19453696068aa5
Akshay Kashyap <the.akshay.kashyap@gmail.com>
2023-09-13 00:17:59 +0530

    Created new app directory

---
+++ apps/CustomDivePlan.cpp
@@ -0,0 +1,18 @@
+#include "core/deco.h"
+#include "core/dive.h"
+#include "core/divelog.h"
+#include "core/file.h"
+#include "core/event.h"
+#include "core/divecomputer.h"
+#include "core/planner.h"
+#include "core/qthelper.h"
+#include "core/subsurfacestartup.h"
+#include "core/units.h"
+#include "core/divelist.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main ()
+{
+	return 0;
+}
